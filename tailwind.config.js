e041719dbee532bf09269a629aa1c9e0e924dce9
fairyhunter13 <fairyhunter13@gmail.com>
2025-09-29 16:06:02 +0700

    feat: add Tailwind CSS build pipeline and precompiled admin.css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./internal/adapter/httpserver/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
